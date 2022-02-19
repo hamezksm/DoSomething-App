0a7e2f3642fdebdbcd5f8d73543b9d39346d58c3
hamezksm <jamcav097@gmail.com>
2022-02-19 19:39:58 +0300

    Second commit

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class boards(models.Model):
+    board_name=models.CharField(max_length=25,unique=True)
+    description=models.TextField(max_length=150)
 class User(models.Model):
     id=models.AutoField(primary_key=True)
     username=models.CharField(unique=True,max_length=15)
