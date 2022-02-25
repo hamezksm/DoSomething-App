66bc70c96569f7d8ac2581d122b18ec98a8b1197
hamezksm <jamcav097@gmail.com>
2022-02-26 02:16:11 +0300

    Add some bootstrap styling and connected the register and login pages

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from app import views 
 from django.urls import path
+from register import views as v
 
 urlpatterns=[
-    path('',views.home,name="home"),
+    path('',views.welcome,name="welcome"),
+    path('register/',v.register, name="register"),
+    path('home/',views.home,name="home"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#takes you to the get started page
+def welcome(request):
+
+    return render(request,'main/welcome.html')
+
+
+#takes you to the home page
 def home(request):
     
     return render(request,'main/home.html')
